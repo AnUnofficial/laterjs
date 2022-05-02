90093d5fcc5f0b75fd9661dbb8e001651cf9bb92
Unofficial <88250679+AnUnoffical@users.noreply.github.com>
2022-05-02 19:45:31 -0400

    jesus that's a lot of stuff

--- later.js
+++ later.js
@@ -1,39 +1,94 @@
 const later = {
-  queue: [ ],
-    get: function ( ) 
-  {
-      return this.queue;
-    },
-  reset: function ( ) 
-  {
-    this.queue = [ ];
-  },
-  add: function ( process ) 
-  {
-    this.queue.push ( process );
-  },
-  multi: function ( process ) 
-  {
-    let len = process.length;
-    for ( let i = 0; i < len; i++ ) 
-    {
-      this.queue.push( process [ i ] );
-    }
-  },
-  run: function ( index ) 
-  {
-    switch ( typeof index ) 
-    {
-      case "number":
-        eval ( queue [ index ] );
-        this.queue.pop ( index );
-      break;
-      default:
-        let len = this.get( ).length;
-        for ( let i = 0; i < len; i++ ) 
-        {
-          eval ( this.queue [ i ] );
-        }
-    this.reset( );
-  }
+queue: [ ],
+threads: [ ],
+ids: [ ],
+get: function ( type ) {
+switch( type ) {
+case 0:
+return this.queue;
+case 1:
+return this.threads;
+case 2:
+return this.ids;
+}
+},
+reset: function ( id, type ) {
+switch ( type ) {
+case 0:
+this.queue = [ ];
+break;
+case 1:
+this.threads = [ ];
+case 2:
+switch ( typeof id ) {
+case "number":
+this.threads [ id ] = [ ];
+default:
+this.ids = [ ];
+}
+break;
+}
+},
+add: function ( id, process ) {
+switch( typeof id ) {
+case "number":
+this.threads [ id ].push ( process );
+break;
+default:
+this.queue.push ( process );
+}
+},
+multi: function ( id, process ) {
+let len = process.length;
+switch ( typeof id ) {
+case "number":
+for ( let i = 0; i < len; i++ ) {
+this.threads [ id ].push ( process [ i ] );
+}
+break;
+default:
+for ( let i = 0; i < len; i++ ) {
+this.queue.push ( process [ i ] );
+}
+}
+},
+run: function ( id, index ) {
+switch( typeof id ) {
+case "number":
+switch( typeof index ) {
+case "number":
+eval(this.threads [ id ] [ index ] );
+this.threads [ id ].pop ( index );
+break;
+default:
+let len = this.get ( 1 ) [ id ].length;
+for ( let i = 0; i < len; i++ ) {
+eval ( this.threads [ id ] [ i ]);
+}
+this.reset ( 1 );
+}
+break;
+default:
+switch( typeof index ) {
+case "number":
+eval ( this.queue [ index ] );
+this.queue.pop ( index );
+break;
+default:
+let len = this.get ( ).length;
+for ( let i = 0; i < len; i++ ) {
+eval( this.queue [ i ] );
+}
+this.reset ( 0 );
+}
+}
+},
+create: function ( id ) {
+switch ( this.ids.includes ( id ) ) {
+case true:
+return;
+}
+this.threads [ id ] = [ ];
+this.ids.push ( id );
+}
 }
