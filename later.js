ef245576a96757005837ef6723bf5ffa11a8f7e4
Unofficial <88250679+AnUnoffical@users.noreply.github.com>
2022-05-02 10:08:56 -0400

    pretty

--- later.js
+++ later.js
@@ -1,31 +1,39 @@
 const later = {
-queue: [],
-get: function() {
-return this.queue;
-},
-reset: function() {
-this.queue = [];
-},
-add: function(process) {
-this.queue.push(process);
-},
-multi: function(process) {
-let len = process.length;
-for(let i = 0; i < len; i++) {
-this.queue.push(process[i]);
-}
-},
-run: function(index) {
-switch(typeof index) {
-case "number":
-eval(queue[index]);
-this.queue.pop(index);
-break;
-default:
-let len = this.get().length;
-for(let i = 0; i < len; i++) {
-eval(this.queue[i]);
-}
-this.reset();
-}
+  queue: [ ],
+    get: function ( ) 
+  {
+      return this.queue;
+    },
+  reset: function ( ) 
+  {
+    this.queue = [ ];
+  },
+  add: function ( process ) 
+  {
+    this.queue.push ( process );
+  },
+  multi: function ( process ) 
+  {
+    let len = process.length;
+    for ( let i = 0; i < len; i++ ) 
+    {
+      this.queue.push( process [ i ] );
+    }
+  },
+  run: function ( index ) 
+  {
+    switch ( typeof index ) 
+    {
+      case "number":
+        eval ( queue [ index ] );
+        this.queue.pop ( index );
+      break;
+      default:
+        let len = this.get( ).length;
+        for ( let i = 0; i < len; i++ ) 
+        {
+          eval ( this.queue [ i ] );
+        }
+    this.reset( );
+  }
 }
