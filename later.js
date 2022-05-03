c7535571184518ba7157faa76c277cc5ceaf8cd4
Unofficial <88250679+AnUnoffical@users.noreply.github.com>
2022-05-02 20:37:40 -0400

    Update later.js

--- later.js
+++ later.js
@@ -1,4 +1,4 @@
-const later = {
+const later = { //The Self Evident Truth: Younger coders like me get some level of laxation from readable / maintainable code. This is beyond that
 queue: [ ],
 threads: [ ],
 ids: [ ],
@@ -6,10 +6,13 @@ get: function ( type ) {
 switch( type ) {
 case 0:
 return this.queue;
+break;
 case 1:
 return this.threads;
+break;
 case 2:
 return this.ids;
+break;
 }
 },
 reset: function ( id, type ) {
@@ -19,6 +22,7 @@ this.queue = [ ];
 break;
 case 1:
 this.threads = [ ];
+break;
 case 2:
 switch ( typeof id ) {
 case "number":
@@ -52,34 +56,34 @@ this.queue.push ( process [ i ] );
 }
 }
 },
-run: function ( id, index ) {
-switch( typeof id ) {
+run: function ( id, index ) { //I'm going to comment here, as later.run is a beast of a function
+switch( typeof id ) { //Check if user wants to run something from a thread
 case "number":
-switch( typeof index ) {
+switch( typeof index ) { //Do they want to run something specific?
 case "number":
-eval(this.threads [ id ] [ index ] );
+eval(this.threads [ id ] [ index ] ); //If so, evaluate the index then pop it
 this.threads [ id ].pop ( index );
 break;
-default:
+default: //User wants to run all of thread
 let len = this.get ( 1 ) [ id ].length;
-for ( let i = 0; i < len; i++ ) {
+for ( let i = 0; i < len; i++ ) { //Loop through all thread, evaluate, and reset
 eval ( this.threads [ id ] [ i ]);
 }
-this.reset ( 1 );
+this.reset ( id, 1 );
 }
 break;
-default:
-switch( typeof index ) {
+default: //User wants to run through main queue
+switch( typeof index ) { //Does the user want to run something specific?
 case "number":
-eval ( this.queue [ index ] );
+eval ( this.queue [ index ] ); //If so, evaluate the index then pop it
 this.queue.pop ( index );
 break;
 default:
 let len = this.get ( ).length;
 for ( let i = 0; i < len; i++ ) {
-eval( this.queue [ i ] );
+eval( this.queue [ i ] ); //Loop through all queue, evaluate, and reset
 }
-this.reset ( 0 );
+this.reset ( "", 0 );
 }
 }
 },
